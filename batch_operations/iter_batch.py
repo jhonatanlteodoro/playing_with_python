27120d0a0340fec9e65f8a69ddafc11525454a61
Jhonatan Teodoro <mollitiamteodoro@gmail.com>
2023-01-25 17:26:37 +0100

    batching using while

--- batch_operations/iter_batch.py
+++ batch_operations/iter_batch.py
@@ -30,3 +30,31 @@ def test_2():
         pass
     # print("--- %s seconds ---" % (time.time() - start_time))
 
+
+def test_3():
+    to_batch = [x for x in range(1, 10000, 2)]
+    batch = []
+    lenght = len(to_batch)
+    times = lenght / 10
+    counter = 0
+    idx = 0
+    while counter < times:
+        batch.append(to_batch[idx: idx+10])
+        counter += 1
+        idx += 10
+    for b in batch:
+        # print(b)
+        pass
+
+
+if __name__ == '__main__':
+    import timeit
+    # print(timeit.timeit("test()", setup="from __main__ import test"))
+
+    # For Python>=3.5 one can also write:
+    print(timeit.timeit("test_1()", number=1000, globals=locals()))
+
+    print(timeit.timeit("test_2()", number=1000, globals=locals()))
+    
+    print(timeit.timeit("test_3()", number=1000, globals=locals()))
+
