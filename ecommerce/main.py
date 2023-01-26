9695e93bdd8d1951d58b58a94c7e884f4523685c
Jhonatan Teodoro <mollitiamteodoro@gmail.com>
2023-01-26 18:21:49 +0100

    improve route to render template

--- ecommerce/main.py
+++ ecommerce/main.py
@@ -1,12 +1,14 @@
 from flask import Flask
+from flask import render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="src/templates")
 
 @app.route("/")
 def home():
-    return "<p>Home</p>"
+    return render_template('home.html')
 
 
 @app.route("/product/<int:product_id>")
 def product_detail(product_id):
-    return f"<p>Product detail {product_id}</p>"
+    return render_template('product_detail.html', product_id=product_id)
+    # return f"<p>Product detail {product_id}</p>"
