6e5ca98a9b4470d5d51f1522952d8bb88324d4f5
Jhonatan Teodoro <mollitiamteodoro@gmail.com>
2023-01-27 08:39:20 +0100

    testing blueprint

--- ecommerce/main.py
+++ ecommerce/main.py
@@ -1,14 +1,15 @@
 from flask import Flask
-from flask import render_template
+from src.routes.blueprints import home_bp, products_bp
 
 app = Flask(__name__, template_folder="src/templates")
+app.register_blueprint(home_bp)
+app.register_blueprint(products_bp)
+# @app.route("/")
+# def home():
+#     return render_template('home.html')
 
-@app.route("/")
-def home():
-    return render_template('home.html')
 
-
-@app.route("/product/<int:product_id>")
-def product_detail(product_id):
-    return render_template('product_detail.html', product_id=product_id)
-    # return f"<p>Product detail {product_id}</p>"
+# @app.route("/product/<int:product_id>")
+# def product_detail(product_id):
+#     return render_template('product_detail.html', product_id=product_id)
+#     # return f"<p>Product detail {product_id}</p>"
